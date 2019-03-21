51a743f43db5545672225a09cae2931cb2c03343
Alex Moon <alex.moon@printed.com>
2019-03-21 17:08:08 +0000

    VGA-206 - don't fail on JSON encode error

--- src/Printed/Bundle/Queue/Entity/QueueTask.php
+++ src/Printed/Bundle/Queue/Entity/QueueTask.php
@@ -386,6 +386,11 @@ class QueueTask implements QueueTaskInterface
      */
     public function setResponseError(string $class, string $message, array $stack)
     {
+        $jsonCheck = json_encode($stack);
+        if (!$jsonCheck) {
+            $stack = json_last_error_msg();
+        }
+
         $this->responseError['class'] = $class;
         $this->responseError['message'] = $message;
         $this->responseError['stack'] = $stack;
